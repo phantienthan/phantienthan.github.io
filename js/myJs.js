5fe9828bd2384ae9fd3c437fcdd217577ee3a2cc
phantienthan <66736019+phantienthan@users.noreply.github.com>
2021-08-16 19:07:40 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
       imageUrl: "img/cuteCat.jpg",
       imageWidth: 300,
       imageHeight: 300,
-      imageborder-radius:50px,
+      border-radius:50%,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
