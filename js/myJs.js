7047c092dbeff0aa886fe5064511c359e5eb0204
phantienthan <66736019+phantienthan@users.noreply.github.com>
2021-08-16 18:52:28 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
   text1: "Hello Cậu !",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ☻ ",
+  text3: "Cậu yêu tớ có phải không 😙 ",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à ?",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text5: "Cậu mơ à ☻",
+  text6: "Yêu ơi là iuuu ♥",
+  text7: "lí do cậu thích tớ đi :v",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp trai vãi nều",
   text10: "Tớ biết mà :D Yêu cậu 300.000",
   text11:
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn ♥",
+  text12: "Oki lunn ♥",
 };
 
 $(document).ready(function () {
